4de064f5d0d5bee3f90ba1a7da8b4488a7ba2f29
Ganlhi <ganlhi@gmail.com>
2020-01-18 14:24:09 +0100

    wip: base ecs components for movement

---
+++ Void Command/Assets/Scripts/Components/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct Position : IComponentData
+{
+    
+    
+    
+}
